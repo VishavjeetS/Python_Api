17cee76f0c8eacbe88c7c3ada24572f978603a34
VishavjeetS <vishavhanspal00@gmail.com>
2022-07-04 21:52:33 +0530

    First Message

---
+++ main.py
@@ -0,0 +1,50 @@
+import datetime as dt
+from typing import Optional
+from fastapi import FastAPI, Path
+from pydantic import BaseModel, Field
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    if_offer: Optional[bool] = None
+
+class TradeDetails(BaseModel):
+    buySellIndicator: str = Field(description="A value of BUY for buys, SELL for sells.")
+
+    price: float = Field(description="The price of the Trade.")
+
+    quantity: int = Field(description="The amount of units traded.")
+
+
+class Trade(BaseModel):
+    asset_class: Optional[str] = Field(alias="assetClass", default=None, description="The asset class of the instrument traded. E.g. Bond, Equity, FX...etc")
+
+    counterparty: Optional[str] = Field(default=None, description="The counterparty the trade was executed with. May not always be available")
+
+    instrument_id: str = Field(alias="instrumentId", description="The ISIN/ID of the instrument traded. E.g. TSLA, AAPL, AMZN...etc")
+
+    instrument_name: str = Field(alias="instrumentName", description="The name of the instrument traded.")
+
+    trade_date_time: dt.datetime = Field(alias="tradeDateTime", description="The date-time the Trade was executed")
+
+    trade_details: TradeDetails = Field(alias="tradeDetails", description="The details of the trade, i.e. price, quantity")
+
+    trade_id: str = Field(alias="tradeId", default=None, description="The unique ID of the trade")
+
+    trader: str = Field(description="The name of the Trader")
+
+
+@app.get("/")
+def read_root():
+    #server stuff
+    return {"Trade_Items" : "Hello World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Trade):
+    return {"item_name": item.name, "item_id": item_id}
